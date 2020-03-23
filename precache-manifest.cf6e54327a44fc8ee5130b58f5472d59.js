1d2b8e3df6fa206a6dbd4c0af64fbeeb42f395c9
Majkel <michal.miszczak13@gmail.com>
2020-03-23 17:47:41 +0100

    Updates

---
+++ precache-manifest.cf6e54327a44fc8ee5130b58f5472d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dcb76420229c71e4faa",
+    "url": "/koronawirus/static/css/main.dbbbc5c5.chunk.css"
+  },
+  {
+    "revision": "8dcb76420229c71e4faa",
+    "url": "/koronawirus/static/js/main.de94066b.chunk.js"
+  },
+  {
+    "revision": "ba1acac997cd17d9fa69",
+    "url": "/koronawirus/static/js/runtime~main.51ef49d6.js"
+  },
+  {
+    "revision": "7bb66b8f8220e08c9c58",
+    "url": "/koronawirus/static/js/2.397fcddf.chunk.js"
+  },
+  {
+    "revision": "bbdf4bcf9c2b48a6c6619a2ae250077c",
+    "url": "/koronawirus/index.html"
+  }
+];
